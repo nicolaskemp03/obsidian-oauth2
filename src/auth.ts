ad48ee29165bc54f245e0a05069ba96fc7ced080
nherreraKx <nherrera@konexa.cl>
2026-06-16 15:30:13 -0400

    fix(auth): remove invalid tenantId property from Auth.js type

--- src/auth.ts
+++ src/auth.ts
@@ -6,7 +6,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     AzureADProvider({
       clientId: process.env.AZURE_AD_CLIENT_ID,
       clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
-      tenantId: process.env.AZURE_AD_TENANT_ID,
       issuer: `https://login.microsoftonline.com/${process.env.AZURE_AD_TENANT_ID}/v2.0`,
       authorization: {
         url: `https://login.microsoftonline.com/${process.env.AZURE_AD_TENANT_ID}/oauth2/v2.0/authorize`,
